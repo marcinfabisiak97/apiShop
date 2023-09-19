85152e55b14787600c09eb8ff67f4f66c89526ac
marcinfabisiak97 <marcinfab1@o2.pl>
2023-09-19 23:57:43 +0200

    changing adress for order

--- routes/sendEmail.ts
+++ routes/sendEmail.ts
@@ -42,7 +42,17 @@ sendMail.post("/sendorderdetails", async (req: Request, res: Response) => {
     from: "needmartinshop@gmail.com",
     to: "needmartinshop@gmail.com",
     subject: "Shipment Confirmation",
-    html: `<p>${JSON.stringify(data)}</p>`,
+    html: `<p>${JSON.stringify(data)}</p>
+    <p>Products: ${JSON.stringify(data.products)}</p>
+    <p>Order value: ${JSON.stringify(data.amount)}</p>
+    <p>First name: ${JSON.stringify(data.firstName)}</p>
+    <p>Last name: ${JSON.stringify(data.lastName)}</p>
+    <p>Street: ${JSON.stringify(data.street)}</p>
+    <p>Flat: ${JSON.stringify(data.flat)}</p>
+    <p>Country: ${JSON.stringify(data.country)}</p>
+    <p>Post Code: ${JSON.stringify(data.postCode)}</p>
+    <p>City: ${JSON.stringify(data.city)}</p>
+    <p>Phone ${JSON.stringify(data.phone)}</p>`,
   };
 
   transporter.sendMail(mailOptions, (error: Error | null, info: any) => {
